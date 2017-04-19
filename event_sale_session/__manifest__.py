40793be3c678ff1b12935adb98df8f210ca29004
David Vidal <chienandalu@gmail.com>
2017-04-19 10:19:38 +0200

    [10.0][WIP] event_sale_session: New module

---
+++ event_sale_session/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 David Vidal<david.vidal@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Event Sale Sessions',
+    'version': '10.0.1.0.0',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    "license": "AGPL-3",
+    'website': 'https://odoo-community.org/',
+    'category': 'Marketing',
+    'summary': 'Sessions sales in events',
+    'depends': [
+        'event_sale',
+        'event_session,'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+}
