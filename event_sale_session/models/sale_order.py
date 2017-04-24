d31e49f96dfa982d18e5ad269021c679c48b4935
David Vidal <chienandalu@gmail.com>
2017-04-24 19:09:19 +0200

    [10.0][WIP] event_sale_session: Remove session_ids

--- event_sale_session/models/sale_order.py
+++ event_sale_session/models/sale_order.py
@@ -11,11 +11,6 @@ class SaleOrderLine(models.Model):
         comodel_name='event.session',
         string='Session',
     )
-    event_session_ids = fields.One2many(
-        comodel_name='event.session',
-        related='event_id.session_ids',
-        readonly=True,
-    )
     event_sessions_count = fields.Integer(
         comodel_name='event.session',
         related='event_id.sessions_count',
