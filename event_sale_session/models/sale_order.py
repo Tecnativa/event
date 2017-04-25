8bce33a05a229d45d89159f14cbd05a5a1fb7f89
David Vidal <chienandalu@gmail.com>
2017-04-25 18:03:18 +0200

    [10.0][WIP] event_sale_session: sale order session button

--- event_sale_session/models/sale_order.py
+++ event_sale_session/models/sale_order.py
@@ -3,6 +3,30 @@
 from odoo import api, fields, models
 
 
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    registration_ids = fields.One2many(
+        comodel_name='event.registration',
+        inverse_name='sale_order_id',
+        string='Attendees',
+        readonly=True,
+    )
+    sale_session_count = fields.Integer(
+        compute ="_compute_sale_sessions",
+        string ="Sessions in this sale order",
+        readonly ="True",
+    )
+
+    @api.multi
+    @api.depends("sale_session_count")
+    def _compute_sale_sessions(self):
+        self.sale_session_count = 0
+        for so_line in self.order_line:
+            if so_line.session_id:
+                self.sale_session_count += 1
+
+
 class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
@@ -14,5 +38,16 @@ class SaleOrderLine(models.Model):
     event_sessions_count = fields.Integer(
         comodel_name='event.session',
         related='event_id.sessions_count',
+        readonly=True,
+    )
+    event_session_seats_available = fields.Integer(
+        related='session_id.seats_available',
+        string='Available Seats',
+        readonly=True,
+    )
+    registration_ids = fields.One2many(
+        comodel_name='event.registration',
+        inverse_name='sale_order_line_id',
+        string='Attendees',
         readonly=True,
     )
