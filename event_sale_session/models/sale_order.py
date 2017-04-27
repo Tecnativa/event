293ba9940c273e8bf1131e486b9b5f59d2ba9481
David Vidal <chienandalu@gmail.com>
2017-04-27 17:58:55 +0200

    [10.0] event_sale_session: check seats availavility

--- event_sale_session/models/sale_order.py
+++ event_sale_session/models/sale_order.py
@@ -38,3 +38,26 @@ class SaleOrderLine(models.Model):
         string='Attendees',
         readonly=True,
     )
+
+    @api.multi
+    def write(self, values):
+        super(SaleOrderLine, self).write(values)
+        for line in self:
+            if not line._session_seats_available():
+                raise ValidationError(_(
+                    "There are sessions with no available seats!\n"
+                    "Edit them so you can save the sale order"))
+
+    @api.onchange('product_uom_qty')
+    def product_uom_change(self):
+        super(SaleOrderLine, self).product_uom_change()
+        if not self._session_seats_available():
+            self.product_uom_qty = 1.0
+            raise UserError(_(
+                "Not enough seats. Change quanty or session"))
+
+    @api.multi
+    def _session_seats_available(self):
+        if self.session_id:
+            if self.event_session_seats_available < self.product_uom_qty:
+                return False
